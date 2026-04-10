33ab18d1ac5af53e3e4fa4299ce0483e67f86273
chocobone <yooho1030@naver.com>
2026-04-10 13:34:59 +0900

    [UPDATE] shell script

--- shell/model_run.sh
+++ shell/model_run.sh
@@ -2,9 +2,9 @@
 #SBATCH -J gambit
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=8
-#SBATCH --mem-per-gpu=29G
-#SBATCH -p batch_ugrad
-#SBATCH -w aurora-g7
+#SBATCH --mem-per-gpu=32G
+#SBATCH -p batch_ugrad_advisor_x 
+#SBATCH -w moana-u8
 #SBATCH -t 1-0
 #SBATCH -o logs/slurm-%A.out
 
@@ -15,5 +15,5 @@ conda activate training
 
 cd /data/yho7374/repos/chess_rust/shell
 ./preprocess.sh
-./training.sh
-./test_rust.sh
+./model_training.sh
+./rust_test.sh
---
+++ shell/preprocess.sh
@@ -0,0 +1,4 @@
+cd /data/yho7374/repos/chess_rust/preprocess
+
+python data_filtering.py
+python tokenize.py
